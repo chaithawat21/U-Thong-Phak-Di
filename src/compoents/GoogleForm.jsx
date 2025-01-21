c4094c7559198dd69535583e3ab10c9eb5b5a0fc
Chai <chaithawat.contact@gmail.com>
2025-01-21 17:03:53 +0700

    updated responsive

--- src/compoents/GoogleForm.jsx
+++ src/compoents/GoogleForm.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 function GoogleForm() {
   return (
-    <div className="container mx-auto p-8 ">
+    <div className="container  py-8 ">
       <div className="w-full flex flex-col items-center ">
         <iframe
           src="https://docs.google.com/forms/d/e/1FAIpQLSdY3eevTp_x5cZxtgNRObNi4MO5ykhU7m-LZL2_cEYvL6D6QQ/viewform?embedded=true"
-          className="w-full h-[1900px] sm:w-[320px] sm:h-[1900px] md:w-[640px] md:h-[1700px] overflow-visble"
+          className="w-full h-[2000px] sm:w-[375px] sm:h-[2000px] md:w-[640px] md:h-[1700px]"
 
         >
           Loading…
